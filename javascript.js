855eff8aeb64328c8132bad4a881f681e8e31500
zanz1bra <talbergaerika@gmail.com>
2024-03-19 12:35:40 +0200

    add playGame functioni to play 5 rounds

--- javascript.js
+++ javascript.js
@@ -1,33 +1,69 @@
 let options = ["Rock", "Paper", "Scissors"];
 
+let tie = "This is a tie!";
+let rockPaper = "You choose poorly! Paper beats Rock!";
+let rockScissors = "You choose wisely! Rock beats Scissors";
+let paperScissors = "You choose poorly! Scissors beat Paper!";
+let paperRock = "You choose wisely! Paper beats Rock";
+let scissorsRock = "You choose poorly! Rock beats Scissors!";
+let scissorsPaper = "You choose wisely! Scissors beat Paper";
+
 function getComputerChoice() {
     return options[Math.floor(Math.random() * options.length)];
 }
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection.toLowerCase() == "rock" && computerSelection == "Rock") {
-        return "This is a tie!";
+        return tie;
     } else if (playerSelection.toLowerCase() == "rock" && computerSelection == "Paper") {
-        return "You choose poorly! Paper beats Rock!";
+        return rockPaper;
     } else if (playerSelection.toLowerCase() == "rock" && computerSelection == "Scissors") {
-        return "You choose wisely! Rock beats Scissors";
+        return rockScissors;
 
     } else if (playerSelection.toLowerCase() == "paper" && computerSelection == "Paper") {
-        return "This is a tie!";
+        return tie;
     } else if (playerSelection.toLowerCase() == "paper" && computerSelection == "Scissors") {
-        return "You choose poorly! Scissors beat Paper!";
+        return paperScissors;
     } else if (playerSelection.toLowerCase() == "paper" && computerSelection == "Rock") {
-        return "You choose wisely! Paper beats Rock";
-        
+        return paperRock;
+
     } else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "Scissors") {
-        return "This is a tie!";
+        return tie;
     } else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "Rock") {
-        return "You choose poorly! Rock beats Scissors!";
+        return scissorsRock;
     } else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "Paper") {
-        return "You choose wisely! Scissors beat Paper";
+        return scissorsPaper;
     }
 }
 
-let playerSelection = prompt("Rock, Paper or Scissors? Choose wisely!");
-let computerSelection = getComputerChoice();
+// let playerSelection = prompt("Rock, Paper or Scissors? Choose wisely!");
+// let computerSelection = getComputerChoice();
 console.log(playRound(playerSelection, computerSelection));
+
+function playGame() {
+    let playerScore = 0;
+    let computerScore = 0;
+
+    for (let round = 1; round <= 5; round++) {
+        console.log("Round: " + round);
+        let playerSelection = prompt("Rock, Paper or Scissors? Choose wisely!");
+        let computerSelection = getComputerChoice();
+        let result = playRound(playerSelection, computerSelection);
+        if (result.includes("You choose wisely")) {
+            playerScore++
+        } else if (result.includes("You choose poorly")) {
+            computerScore++
+        } else {
+            console.log(tie)
+        }
+    }
+
+
+    if (playerScore > computerScore) {
+        console.log("You win! Great job!");
+    } else {
+        console.log("You lose! But at least you still have a face! :)")
+    }
+}
+
+playGame();
